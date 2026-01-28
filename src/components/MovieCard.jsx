8201cd5e04cc8e2d4e09a552a51c4cd1fc43b606
ferdaous-wahid <ferdaouswahid2001@gmail.com>
2026-01-28 18:27:07 +0600

    practicing react

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+export default function MovieCard({ movie }) {
+  function onFavoriteButton() {
+    alert("clicked");
+  }
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt="Can't found" />
+        <div className="movie-overlay">
+          <button
+            className="favorite-button"
+            onClick={onFavoriteButton}
+          ></button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const movies = [
+    { id: 1, title: "John wick", release_date: "2020" },
+    { id: 2, title: "Matrix", release_date: "2021" },
+    { id: 3, title: "Freeman", release_date: "2022" },
+    { id: 4, title: "Paradise", release_date: "2023" },
+  ];
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies... "
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movie-grid">
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            ),
+        )}
+      </div>
+    </div>
+  );
+}
